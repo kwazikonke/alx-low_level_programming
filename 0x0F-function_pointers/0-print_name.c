0f56f977e9082b5c7181512ab7e1faf63f655add
Kwazikonke <kwazikonke.mbatha05@gmail.com>
2023-04-19 11:06:22 -0700

    OxOF.C

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: name to pass to function
+ * @f: function
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void  (*f)(char *))
+{
+	if (f != NULL)
+		f(name);
+}
