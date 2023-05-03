3c7dff6c4cf6ffd0787cf4d408ffcfa023b0093f
Kwazikonke <kwazikonke.mbatha05@gmail.com>
2023-05-03 09:59:50 -0700

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * dlistint_len - print the length of a doubly linked list h
+ * @h: head of the list
+ *
+ * Return: The number of nodes in the list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
